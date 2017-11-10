42bf8ad03505c4fc153012ef1d7cb73ec5154fba
from Raspberry <raspberry@villach.at>
2017-11-10 07:57:54 +0000

    server changes

--- Development/testing/test.js
+++ Development/testing/test.js
@@ -1,97 +1,28 @@
 var http = require('http');
-var mysql = require('mysql');
+var port = 1235;
+var qs = require('querystring');
 
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "gressl123",
-  database: "Supermarkt_Verwaltung"
-});
-
-
-con.connect(function(error) {
-	
-    http.createServer(function (req, res) {
-        try{
-            var tablename = getTableName(req.url);
+http.createServer(function (request, response) {
+    
+    console.log(request);
+    if (request.method == "POST") {
+           var body = [];
+           request.on('data', function (chunk) {
+         body.push(chunk);
+            }).on('end', function () {
+         body = Buffer.concat(body).toString();
+        // at this point, `body` has the entire request body stored in it as a string
+            console.log(body);
 			
+			response.writeHead(200, { 'Content-Type': 'text/plain' });
+	     response.write("" + JSON.stringify(body));
+         response.end("");
 			
-            if (getTableName(req.url) != undefined) {
-                
-                con.query("SELECT * FROM " + capitalizeFirstLetter(tablename), function (err, result, fields) {
-                    if (err || error) {
-                        res.writeHead(400, { 'Content-Type': 'text/plain' });
-					res.write(err + "\n\n\n" + error);
-                        res.end();
-                    }
-                    else {
-                        res.writeHead(200, { 'Content-Type': 'text/plain' });
+			});
 
-                        for (var key in result[0]) {
-                            res.write(key + "                        ");
-                        }
-                        res.write("\n");
-
-                        for (var key in result[0]) {
-                            res.write("----------------------------------");
-                        }
-
-                        res.write("\n");
-
-                        for (var key in result) {
-                            res.write("\n");
-                            for (var valkey in result[key]) {
-
-                                res.write(result[key][valkey] + "                      ");
-
-                            }
-                            res.write("\n");
-                        }
-                        res.end();
-                       }
-                    });
-                }
-                //res.write(JSON.stringify(result));
-                else {
-                res.write("invalid parameters");
-                res.end();
-            }
-	           
+	     
     }
-    catch(ex){
-       
-    }
+    
+   
 	
-	}).listen(1234); 
-});
-
-
-function getTableName(requesturl) {
-	try {
- var all = requesturl.split("?");
-	if(all != undefined){
-		
-	//console.log("all[1]:" + all[1]);
-	var split_1 = all[1].split("=");
-	//console.log("split_1:" + split_1);
-	
-	if(split_1[0].toLowerCase() == "table" && split_1[1] !== undefined){
-		return split_1[1];
-	}
-	else{s
-		return undefined;
-	}
-	}
-	else{
-		return undefined;
-	}
-	
-} catch (err) {
-	return undefined;
-} 
-}
-
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
+}).listen(port);
