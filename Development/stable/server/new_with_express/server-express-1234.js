8e037bc6ef177663b8e766715008d46f8ec8c341
from Raspberry <raspberry@villach.at>
2017-11-15 07:41:55 +0000

    server cahnges

--- Development/stable/server/new_with_express/server-express-1234.js
+++ Development/stable/server/new_with_express/server-express-1234.js
@@ -14,6 +14,70 @@ var con = mysql.createConnection({
 //helperlib.initResponse(res);
 con.connect();
 
+app.get('/login', function (req, res) {
+     try {
+		 
+		  var auth_token = req.headers.authorization.split(" ")[1];
+			console.log(auth_token);
+
+			var buffer = new Buffer(auth_token, 'base64')
+			var credentials = buffer.toString().split(":");
+
+         var query = "select count(*) from Mandatar where M_ID = \'" + credentials[0] + "\' AND Passwort = \'" + credentials[1] + "\'";
+         con.query(query, function (err, result, fields) {
+
+            if (err) {
+                fireResponse(res, 500, "false");
+            }
+            else {
+
+                if (result[0]["count(*)"] == "1") {
+					fireResponse(res, 200, "true");
+                }
+                else {
+                    fireResponse(res, 401, "false");
+                }
+
+            }
+         });
+    } catch (e) {
+        fireResponse(res, 500, "false");
+    }
+});
+
+app.use(function(req,res, next){
+	
+	 try {
+		 
+		  var auth_token = req.headers.authorization.split(" ")[1];
+			console.log(auth_token);
+
+			var buffer = new Buffer(auth_token, 'base64')
+			var credentials = buffer.toString().split(":");
+
+         var query = "select count(*) from Mandatar where M_ID = \'" + credentials[0] + "\' AND Passwort = \'" + credentials[1] + "\'";
+         con.query(query, function (err, result, fields) {
+
+            if (err) {
+                fireResponse(res, 500, "something went wrong.");
+            }
+            else {
+
+                if (result[0]["count(*)"] == "1") {
+                    next();
+                }
+                else {
+                    fireResponse(res, 401, "unauthorized");
+                }
+
+            }
+         });
+    } catch (e) {
+        fireResponse(res, 400, "credentials are needed!");
+    }
+	
+});
+
 app.get('/', function (req, res) {
     fireResponse(res,200, "Server is running!")
 });
@@ -22,43 +86,8 @@ app.get('/get', function (req, res) {
     fireResponse(res, 200, "/get " + req.query.table);
 });
 
-app.get('/login', function (req, res) {
-    
-    var auth_token = req.headers.authorization.split(" ")[1];
-    console.log(auth_token);
-
-    var buffer = new Buffer(auth_token, 'base64')
-    var credentials = buffer.toString().split(":");
-
-    console.log(credentials);
-    
-    var success = checkCredentials(credentials);
-    fireResponse(res, success.httpcode, "" + success.result);
-    
-});
-
 function checkCredentials(credentials) {
-    try {
-         var query = "select count(*) from Mandatar where M_ID = \'" + credentials[0] + "\' AND Passwort = \'" + credentials[1] + "\'";
-         con.query(query, function (err, result, fields) {
-
-            if (err) {
-                return {"httpcode": 500, "result" : false};
-            }
-            else {
-
-                if (result[0]["count(*)"] == "1") {
-                    return { "httpcode": 200, "result": true };
-                }
-                else {
-                    return { "httpcode": 401, "result": false };
-                }
-
-            }
-         });
-    } catch (e) {
-        return { "httpcode": 500, "result": false };
-    }
+   
 }
 
 function fireResponse(res,httpcode, message) {
