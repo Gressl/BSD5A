f258960f2d77605f35d07114812c08ec14e8c550
Gressl <tgressl8@gmail.com>
2018-04-06 08:34:23 +0200

    changes

--- Development/stable/server/new_with_express/server-express-1234.js
+++ Development/stable/server/new_with_express/server-express-1234.js
@@ -452,7 +452,7 @@ try{
 });
 
 //UPDATE
-/*
+
 app.patch('/update', function(req, res) {
 try{
 	console.log(req.body);
@@ -462,7 +462,6 @@ try{
 		fireResponse(res, 400, "no table specified!");
 	} 
 	else{
-		
 		
 		switch (req.body.table.toLowerCase()) {
 			
@@ -522,25 +521,15 @@ try{
 			 }
 			 
 			break;
-			
+			/*
 			case "lagerentnahme":
 			//I_ID, S_ID, ItemMenge, AktuellerPreis
 			try{
 				
 				if(req.body.I_ID && req.body.S_ID && req.body.ItemMenge && req.body.AktuellerPreis){
-					 var query = "insert into Lagerentnahme(I_ID, S_ID, ItemMenge, AktuellerPreis) values(\'"  + req.body.I_ID + "\', \'" + req.body.S_ID + "\', \'" + req.body.ItemMenge + "\', \'" + req.body.AktuellerPreis + "\')";
-			 
-			 console.log(query);
-	             con.query(query, function (err, result, fields) {
+				//var query = "insert into Lagerentnahme(I_ID, S_ID, ItemMenge, AktuellerPreis) values(\'"  + req.body.I_ID + "\', \'" + req.body.S_ID + "\', \'" + req.body.ItemMenge + "\', \'" + req.body.AktuellerPreis + "\')";
 				
-                        if (err) {
-                            fireResponse(res, 500, "error");
-                        }
-                        else { 
-							var ret = createJSONGetTableResponse(result);
-							fireResponse(res,200, ret);
-                        }
-                    });	
+			 
 				}
 				else{
 					fireResponse(res, 500, "params are missing or wrong");
@@ -559,37 +548,7 @@ try{
 				
 				if(req.body.Name && req.body.Preis && req.body.Menge){
 					
-					con.query("select I_ID from Lageritem order by I_ID desc limit 1", function (err, result, fields) {
-				
-                        if (err) {
-                            fireResponse(res, 500, "error");
-                        }
-                        else { 
-						
-							var oldstring = result[0].I_ID;
-							var oldid = oldstring.substring(2,4);
-							var newid = parseInt(oldid);
-							newid++;
-							
-							var newstring = "LI0" + newid;
-							
-							console.log(newid);
-							
-							console.log(result[0]);
-							var query = "insert into Lageritem(I_ID, Name, Preis, Menge) values(\'"  + newstring + "\', \'" + req.body.Name + "\', \'" + req.body.Preis + "\', \'" + req.body.Menge + "\')";
-							console.log(query);
-							con.query(query, function (err1, result, fields) {
-				
-							if (err) {
-								fireResponse(err1, 500, "error");
-							}
-							else { 
-								var ret = createJSONGetTableResponse(result);
-								fireResponse(res,200, ret);
-							}
-							});	
-                        }
-				});	
+					
 				}
 				else{
 					fireResponse(res, 500, "params are missing or wrong");
@@ -605,31 +564,7 @@ try{
 			//S_ID, Verkaufsdatum, KundenID, MandatarID
 			try{
 				 if(req.body.Verkaufsdatum && req.body.KundenID && req.body.MandatarID){
-				  con.query("select S_ID from Verkauf order by S_ID desc limit 1", function (err, result, fields) {
-				
-                        if (err) {
-                            fireResponse(res, 500, "error (DB)");
-                        }
-                        else { 
-								var oldstring = result[0].S_ID;
-								var oldid = oldstring.substring(2,4);
-								var newid = parseInt(oldid);
-								newid++;
-							
-							   var newstring = "SA0" + newid;
-							
-							
-								con.query("insert into Verkauf(S_ID, Verkaufsdatum, KundenID, MandatarID) values(\'"  + newstring + "\', \'" + req.body.Verkaufsdatum + "\', \'" + req.body.KundenID + "\', \'" + req.body.MandatarID + "\')", function (err1, result1, fields) {
-				
-								if (err1) {
-									fireResponse(res, 500, "error (DB)");
-								}
-								else { 
-									fireResponse(res,200, result1);
-								}
-								});		
-                        }
-                    });		
+						
 			 } 
 			 else{
 				  fireResponse(res, 400, "some params are missing!");
@@ -645,32 +580,7 @@ try{
 			//Rechnungsnummer, schonbezahlt, S_ID
 			try{
 				 if(req.body.schonbezahlt && req.body.S_ID ){
-				  con.query("select Rechnungsnummer from Rechnung order by Rechnungsnummer desc limit 1", function (err, result, fields) {
-				
-                        if (err) {
-                            fireResponse(res, 500, "error (DB)");
-                        }
-                        else { 
-								var oldstring = result[0].Rechnungsnummer;
-								var oldid = oldstring.substring(2,4);
-								var newid = parseInt(oldid);
-								newid++;
-							
-							   var newstring = "AT0" + newid;
-							
-							
-								con.query("insert into Rechnung(Rechnungsnummer, schonbezahlt, S_ID) values(\'"  + newstring + "\', \'" + req.body.schonbezahlt + "\', \'" + req.body.S_ID + "\')", function (err1, result1, fields) {
-				
-								if (err1) {
-									fireResponse(res, 500, "error (DB)");
-									console.log(err1);
-								}
-								else { 
-									fireResponse(res,200, result1);
-								}
-								});		
-                        }
-                    });		
+				  	
 			 } 
 			 else{
 				  fireResponse(res, 400, "some params are missing!");
@@ -682,6 +592,7 @@ try{
 			
 			break;
 			
+			*/
 			
 			default:
 				fireResponse(res, 400, "error");
@@ -695,7 +606,7 @@ try{
     fireResponse(res, 500, "oops, something went wrong");
 }
 });
-*/
+
 
 
 app.get('/', function (req, res) {
